7fdd7e1b0a0fd5c1914d702e751dafdd09a40a9c
南山 <south_think@qq.com>
2025-10-16 17:19:43 +0800

    更新版本号到1.1.0

--- HMCL/build.gradle.kts
+++ HMCL/build.gradle.kts
@@ -16,8 +16,8 @@ val isOfficial = System.getenv("HMCL_SIGNATURE_KEY") != null
         || (System.getenv("GITHUB_REPOSITORY_OWNER") == "HMCL-dev" && System.getenv("GITHUB_BASE_REF")
     .isNullOrEmpty())
 
-val buildNumber = "11"
-val versionRoot = System.getenv("VERSION_ROOT") ?: "1.0"
+val buildNumber = "0"
+val versionRoot = System.getenv("VERSION_ROOT") ?: "1.1"
 //val versionType = System.getenv("VERSION_TYPE") ?: if (isOfficial) "nightly" else "unofficial"
 val versionType = System.getenv("VERSION_TYPE") ?: "stable"
 
